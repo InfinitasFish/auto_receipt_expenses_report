61f5530c04392974b6a7ea812a6fa54ac86645af
Шишаев Вячеслав Алексеевич <vshishaev@ussc.ru>
2024-03-20 15:50:35 +0500

    some new exel functionality

--- additional_utils.py
+++ additional_utils.py
@@ -1,87 +1,8 @@
 import openpyxl
+from openpyxl.utils import get_column_letter, column_index_from_string
 import numpy as np
 import cv2
 from PIL import Image
-
-
-def separate_images(np_image, ocr_engine):
-    # TODO: To delete this func?
-
-    bounds = ocr_engine.readtext(np_image)
-    pil_image = Image.fromarray(np_image)
-
-    dots_coner = []
-    dots_sered = []
-
-    for bound in bounds:  # сохранение двух точек бокса
-        p0, p1, p2, p3 = bound[0]
-        dots_coner.append(p0)
-        dots_coner.append(p1)
-        dot = [(p0[0] + p2[0]) / 2, (p0[1] + p2[1]) / 2]
-        dots_sered.append(dot)
-
-    k_ans = 0
-    projection_dot = []
-
-    for j in dots_coner:  # прорекция на прямую
-        y = (k_ans * j[0] - k_ans * k_ans * j[1]) / (1 - k_ans * k_ans)
-        if k_ans == 0:
-            x = j[0]
-        else:
-            x = y / k_ans
-        dot = [x, y]
-        projection_dot.append(dot)
-
-    x = []
-    g = 0
-    right = []
-    left = []
-    perep = [] + [0]
-
-    for i in projection_dot:
-        x = x + [i[0]]
-
-    x.sort()
-
-    l = x[len(x) - 1]
-    r = l / len(x)
-
-    for i in range(0, l, 15):  # подсчет количества точек возле прямой
-        rcount = 0
-        lcount = 0
-        for j in x:
-            if (j < i + 15) and (j >= i - 15):
-                lcount = lcount + 1
-        left = left + [lcount]
-
-    for i in range(len(left)):  # нахождение самых больших скоплений точек
-        if left[i] >= 17:
-            perep = perep + [i]
-
-    X = np.array(dots_coner)
-    count = 0
-    gr = []
-
-    dotss = x
-
-    x, y = pil_image.size
-    perep = perep + [x / 15]
-    for i in range(len(perep)):  # фильтрация
-        if i != 0:
-            if perep[i] - perep[i - 1] < 5:
-                count = count + 1
-            else:
-                gr = gr + [(perep[i - 1] + perep[i - 1 - count]) / 2]
-                count = 0
-        if i == len(perep) - 1:
-            gr = gr + [(perep[i] + perep[i - count]) / 2]
-
-    corp_imgs = []
-    for i in range(1, len(gr)):  # обрезка изображений
-        corp_imgs = corp_imgs + [pil_image.crop((gr[i - 1] * 15, 0, gr[i] * 15, y))]
-
-    # TODO: сделать фильтрацию мусора
-    return corp_imgs, gr
 
 
 def parse_extracted_data(extracted_data):
@@ -143,7 +64,6 @@ def parse_extracted_data(extracted_data):
             elif 'I' in extracted_data[i][0] and curr_total != []:
                 curr_total.append(extracted_data[i][1])
 
-
     # append last sequence
     seqs_dict['address_seqs'].append(curr_address)
     seqs_dict['company_seqs'].append(curr_company)
@@ -156,54 +76,131 @@ def parse_extracted_data(extracted_data):
                      'date': max([' '.join(seqs) for seqs in seqs_dict['date_seqs']], key=len),
                      'total': max([' '.join(seqs) for seqs in seqs_dict['total_seqs']], key=len)}
 
-    print(seqs_dict['total_seqs'])
-
     return entities_dict
 
 
 def fill_exel_report(extracted_data_list, report_filename='exel_report.xlsx'):
-    # extracted data list contains data for every receipt on the image
+    # extracted_data_list contains data for every receipt on the image
     book = openpyxl.open(report_filename, read_only=False)
     sheet = book.worksheets[0]
 
-    # list for storing number of table rows of receipts
-    table_numbers = []
+    # find indexes of the top corners of the table
+    # values of the corner cells is hardcoded
+    left_corner_value = 'Номер по порядку'.lower()
+    right_corner_value = 'Расход (указать договор / ЦЗ)'.lower()
 
-    # 68 is a magic number
+    left_char, left_index, right_char, right_index = find_table_corners_by_two_values((left_corner_value,
+                                                                                      right_corner_value), sheet)
+
+    table_numbers = []
     for i, row in enumerate(sheet):
-        if number := sheet['B' + str(68 + i)].value:
+        if number := sheet[left_char + str(left_index+4 + i)].value:  # 4 is magic number
             table_numbers.append(number)
         else:
             break
 
-    table_number_border = max(table_numbers)
+    table_number_top_border = min(table_numbers)
+    table_number_bottom_border = max(table_numbers)
 
-    # row 82-86 is subscription structure (should copy it)
+    # TODO:
+    # row 82-86 is subscription structure. should copy it, and if there is now free space in table
+    # lower it one row, add new row to the table
+    subscription_structure = []
+    # shape is row x columns, value is cell info tuple (value, style, range (width, height) [if merged cell] )
+    # for i in range(82, 86):
+    #     subscription_structure.append([])
+    #     for j in range(1, 30):
+    #         pass
+
+    cell = sheet['b64']
+    for rng in sheet.merged_cells.ranges:
+        if cell.coordinate in rng:
+            print(rng.__str__().split(':'))
+            print(dir(rng))
+    else:
+        rng = None
+    print(sheet.merged_cells.ranges)
+
+    print(cell)
+    print(cell.value)
+    print(cell.coordinate)
+    print(dir(cell))
+
+    # TODO: в будущем научиться детектить и заполнять номер чека, пока представим, что он всегда 59
+    # copying contract cell to each row, because it's always the same for one report
+    contract_cell_column_value = sheet[right_char + str(right_index+4)].value
+    receipt_number_value = 59
+
+    field_to_column_index_map = {'order_number': 0, 'date': 2, 'receipt_number': 4, 'company': 6,
+                                 'total': 10, 'total_accepted': 13, 'debit': 16, 'contract': 19}
+
+    #
+    # for data in extracted_data_list:
+    #     entities_dict = parse_extracted_data(data)
+    #
+    #     filled_row = False
+    #     for i in range(table_number_bottom_border):
+    #         date_column_char = get_column_letter(column_index_from_string(left_char) + field_to_column_index_map['date'])
+    #         if sheet[date_column_char + str(left_index + i+4)].value == None:
+    #
+    #             number_column_char = get_column_letter(column_index_from_string(left_char) + field_to_column_index_map['receipt_number'])
+    #             company_column_char = get_column_letter(column_index_from_string(left_char) + field_to_column_index_map['company'])
+    #             total_column_char = get_column_letter(column_index_from_string(left_char) + field_to_column_index_map['total'])
+    #             contract_column_char = get_column_letter(column_index_from_string(left_char) + field_to_column_index_map['contract'])
+    #
+    #             print(left_index + i+4)
+    #
+    #             sheet[date_column_char + str(left_index + i+4)] = entities_dict['date']
+    #             sheet[number_column_char + str(left_index + i+4)] = receipt_number_value
+    #             sheet[company_column_char + str(left_index + i+4)] = entities_dict['company']
+    #             sheet[total_column_char + str(left_index + i+4)] = entities_dict['total']
+    #             sheet[contract_column_char + str(left_index + i+4)] = contract_cell_column_value
+    #
+    #             filled_row = True
+    #             break
+    #     if not filled_row:
+    #         pass
+    #         # TODO: should extend table and also lower subscription structure
+    #
+    #     print(filled_row)
+
+    book.save(report_filename)
+    book.close()
 
 
-    for data in extracted_data_list:
-        entities_dict = parse_extracted_data(data)
+def find_table_corners_by_two_values(corner_cell_values, sheet):
+    # search for specific table values in the report
+    # returns two (character, index) tuples of the cells with that values
 
-        # TODO: STOPPED HERE HELLLOOOOO
-        filled_row = False
-        for i in range(table_number_border):
-            if sheet['D' + str(68 + i)].value == None:
-                sheet['D' + str(68 + i)] = entities_dict['date']
-                sheet['H' + str(68 + i)] = entities_dict['company']
-                sheet['L' + str(68 + i)] = entities_dict['total']
-                # TODO: в будущем научиться детектить и заполнять номер чека
-                filled_row = True
-                break
-        if not filled_row:
-            pass
-            # TODO: should extend table and also lower subscription structure
+    left_value, right_value = corner_cell_values[0], corner_cell_values[1]
+    left_char, right_char = '', ''
+    left_index, right_index = -1, -1
 
-    # book.save(report_filename)
-    # book.close()
+    # search left corner by scanning rows and columns
+    # 'ABCDE' and 1-100 row range are magic numbers
+    for c in 'ABCDE':
+        for i in range(1, 100):
+            cell_value = str(sheet[c + str(i)].value)
+            if cell_value != str(None) and cell_value.strip().lower() == left_value:
+                left_char = c
+                left_index = i
+
+    if left_index == -1:
+        return left_char, left_index, right_char, right_index
+
+    # search right corner by scanning columns from left-corner row
+    for i in range(column_index_from_string(left_char) + 1, 100):  # 100 is a magic number
+        cell_value = str(sheet[get_column_letter(i) + str(left_index)].value)
+        if cell_value != str(None) and cell_value.strip().lower() == right_value:
+            right_char = get_column_letter(i)
+            right_index = left_index
+
+    return left_char, left_index, right_char, right_index
 
 
 if __name__ == '__main__':
 
+    # just some testing
     nerv = [('I-ADDRESS', '<s>'),
             ('B-ADDRESS', '115114'),
             ('I-ADDRESS', 'Mockba'),
@@ -232,9 +229,9 @@ if __name__ == '__main__':
             ('I-COMPANY', 'Инфик'),
             ('B-COMPANY', 'Мышь')]
 
-    result = parse_extracted_data(nerv)
-    for k, v in result.items():
-        print(f'{k}:{v}')
+    # cool
+    # result = parse_extracted_data(nerv)
+    # for k, v in result.items():
+    #     print(f'{k}:{v}')
 
-    fill_exel_report([result])
-
+    fill_exel_report([nerv])
